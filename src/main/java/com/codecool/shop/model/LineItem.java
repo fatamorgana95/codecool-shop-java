98bc65014f962ac8e677c16d0318254fcf2f62bb
fatamorgana95 <verabertalan@gmail.com>
2020-11-17 13:08:08 +0100

    Implement LineItem class

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -1,2 +1,37 @@
-package com.codecool.shop.model;public class LineItem {
+package com.codecool.shop.model;
+
+public class LineItem extends BaseModel{
+
+    private int quantity = 0;
+    private int totalPrice = 0;
+
+    private final Product product;
+
+    public LineItem(Product product, String name) {
+        super(name);
+        this.product = product;
+        increaseQuantity();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void increaseQuantity() {
+        this.quantity ++;
+        this.totalPrice += product.getDefaultPrice();
+    }
+
+    public void decreaseQuantity() {
+        this.quantity --;
+        this.totalPrice -= product.getDefaultPrice();
+    }
+
+    public boolean isClearable() {
+        return (quantity < 1);
+    }
 }
